f0b63b4bca1713b6fb00e3214dd036da67b41d9a
riataso <rarararai0070@gmail.com>
2023-07-08 19:23:08 +0900

    :tada: first commit

---
+++ repository-search-app/View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  HomePageView.swift
+//  repository-search-app
+//
+//  Created by 佐藤来 on 2023/07/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
