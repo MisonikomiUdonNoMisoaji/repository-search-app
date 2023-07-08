beb7a92efff681e0d527627b358941082e2a8aac
riataso <rarararai0070@gmail.com>
2023-07-08 17:02:43 +0900

    Initial Commit

---
+++ repository-search-app/repository_search_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  repository_search_appApp.swift
+//  repository-search-app
+//
+//  Created by 佐藤来 on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct repository_search_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
